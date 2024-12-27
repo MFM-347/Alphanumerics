4bbfe52d00853439d4121238de55e9177ddbcfd5
Farhan Madni <madnifm347@gmail.com>
2024-12-27 21:02:59 +0500

    upload

---
+++ src/an.ts
@@ -0,0 +1,40 @@
+const encode = (text: string): string => {
+  if (!text) {
+    return "ERROR: text not found!";
+  }
+  return text
+    .split(" ")
+    .map((word) =>
+      word
+        .toLowerCase()
+        .split("")
+        .map((char) => {
+          const charCode = char.charCodeAt(0);
+          return charCode >= 97 && charCode <= 122 ? charCode - 96 : "";
+        })
+        .filter((n) => n)
+        .join("-"),
+    )
+    .join(" ");
+};
+const decode = (encodedText: string): string => {
+  if (!encodedText) {
+    return "ERROR: encodedText not found!";
+  }
+  return encodedText
+    .split(" ")
+    .map((word) =>
+      word
+        .split("-")
+        .map((num) => {
+          const charCode = Number(num) + 96;
+          return charCode >= 97 && charCode <= 122
+            ? String.fromCharCode(charCode)
+            : "";
+        })
+        .join(""),
+    )
+    .join(" ");
+};
+
+export { encode, decode };
---
+++ src/decode.test.ts
@@ -0,0 +1,8 @@
+import { test, expect } from "vitest";
+import { decode } from "./main";
+
+let encodedText = "8-5-12-12-15 23-15-18-12-4";
+
+test("convert alphanumeric string to text", () => {
+  expect(decode(encodedText)).toBe("hello world");
+});
